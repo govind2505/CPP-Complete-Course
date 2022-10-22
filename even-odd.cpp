2e92ed9b082bbc27addc7f8b567c2f5d800bbf1e
Devansh <mrdevdhingra@gmail.com>
2022-10-22 02:58:41 +0000

    add even and odd

---
+++ even-odd.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+ int main(){
+    
+    for(int i=0;i<=10;i++){
+        if(i%2==0){
+            cout<<"the number is even "<<i<<endl;
+        }
+        else{
+            cout<<"the number is odd "<<i<<endl;
+        }
+    }
+     return 0;
+     
+ }
